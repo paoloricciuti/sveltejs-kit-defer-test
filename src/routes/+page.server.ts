ed04ab2c5dc0d6da25b043f6402946e7fb8b1771
paoloricciuti <ricciutipaolo@gmail.com>
2023-02-21 16:38:21 +0100

    1 file modified

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,22 @@
 const wait = (ms) => new Promise((r) => setTimeout(r, ms));
 
 async function getBlog() {
-	await wait(5000);
+	await wait(2000);
 	return 'A cool blog post';
 }
 
 async function getComments() {
-	await wait(9000);
+	await wait(4000);
 	return ['Very cool', 'Super', 'Fantastic'];
 }
 
 async function getRecommended() {
-	await wait(3000);
+	await wait(1000);
 	return ['Another post', 'Suh interesting'];
 }
 
 async function getFail() {
-	await wait(15000);
+	await wait(5000);
 	throw new Error('Dang!');
 }
 
@@ -28,8 +28,8 @@ function logAndReturn(name: string) {
 }
 
 export async function load() {
-	const recommended = getRecommended().then(logAndReturn('recommended'));
-	const comments = getComments().then(logAndReturn('comments'));
+	const recommended = getRecommended();
+	const comments = getComments();
 	const fail = getFail();
 	const blog = await getBlog();
 	return {
